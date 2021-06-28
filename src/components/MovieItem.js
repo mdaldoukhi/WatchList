b170477f7f4516d8aa72a6b976f874eead620219
mohalenezi <ot.m.alenezi@gmail.com>
2021-06-28 17:27:12 +0300

    add image

--- src/components/MovieItem.js
+++ src/components/MovieItem.js
@@ -4,38 +4,45 @@ import movieStore from "../stores/movieStore";
 import { observer } from "mobx-react";
 /* Styles */
 import { ItemWrapper, ActionTools } from "../styles";
+// usestate
+import { useState } from "react";
 
 const MovieItem = (props) => {
-    /* will change the watched to unwatch */
-    const handleClick = () => {
-        movieStore.updateStatus(props.movie);
-    };
-    /* will delete the movie from list */
-    const deleteClick = () => {
-        movieStore.deleteMovies(props.movie.id);
-    };
-    return (
-        /* Wrap the Item */
-        <ItemWrapper>
-            <p>{props.movie.name}</p>
-            {/* Buttons section */}
-            <ActionTools>
-                <button
-                    onClick={handleClick}
-                    type="button"
-                    className="btn btn-outline-success"
-                >
-                    {(props.movie.status === "watched") ? "Unwatched" : "Watched"}
-                </button>
-                <button
-                    onClick={deleteClick}
-                    type="button"
-                    className="btn btn-outline-danger"
-                >
-                    Delete
-                </button>
-            </ActionTools>
-        </ItemWrapper>
-    );
+  /* will change the watched to unwatch */
+  const handleClick = () => {
+    movieStore.updateStatus(props.movie);
+  };
+  /* will delete the movie from list */
+  const deleteClick = () => {
+    movieStore.deleteMovies(props.movie.id);
+  };
+
+  //usestate add image button
+  const [isHide, setIshide] = useState(0);
+
+  return (
+    /* Wrap the Item */
+    <ItemWrapper>
+      {isHide ? <img src={props.movie.image} /> : false}
+      <p onClick={() => setIshide(!isHide)}>{props.movie.name}</p>
+      {/* Buttons section */}
+      <ActionTools>
+        <button
+          onClick={handleClick}
+          type="button"
+          className="btn btn-outline-success"
+        >
+          {props.movie.status === "watched" ? "Unwatched" : "Watched"}
+        </button>
+        <button
+          onClick={deleteClick}
+          type="button"
+          className="btn btn-outline-danger"
+        >
+          Delete
+        </button>
+      </ActionTools>
+    </ItemWrapper>
+  );
 };
 export default observer(MovieItem);
--- src/movies.js
+++ src/movies.js
@@ -1,43 +1,59 @@
 const movies = [
-    {
-        id: 1,
-        name: "The Lion King",
-        status: "watched",
-    },
-    {
-        id: 2,
-        name: "Troy",
-        status: "watched",
-    },
-    {
-        id: 3,
-        name: "Matrix",
-        status: "watched",
-    },
-    {
-        id: 4,
-        name: "Avatar",
-        status: "watched",
-    },
-    {
-        id: 5,
-        name: "Escape room",
-        status: "watchlist",
-    },
-    {
-        id: 6,
-        name: "Fast and Furious",
-        status: "watchlist",
-    },
-    {
-        id: 7,
-        name: "Awake",
-        status: "watchlist",
-    },
-    {
-        id: 8,
-        name: "Fatherhood",
-        status: "watchlist",
-    },
+  {
+    id: 1,
+    name: "The Lion King",
+    status: "watched",
+    image:
+      "https://www.mcicon.com/wp-content/uploads/2021/01/Music_Movie_1-copy-2.jpg",
+  },
+  {
+    id: 2,
+    name: "Troy",
+    status: "watched",
+    image:
+      "https://www.mcicon.com/wp-content/uploads/2021/01/Music_Movie_1-copy-2.jpg",
+  },
+  {
+    id: 3,
+    name: "Matrix",
+    status: "watched",
+    image:
+      "https://www.mcicon.com/wp-content/uploads/2021/01/Music_Movie_1-copy-2.jpg",
+  },
+  {
+    id: 4,
+    name: "Avatar",
+    status: "watched",
+    image:
+      "https://www.mcicon.com/wp-content/uploads/2021/01/Music_Movie_1-copy-2.jpg",
+  },
+  {
+    id: 5,
+    name: "Escape room",
+    status: "watchlist",
+    image:
+      "https://www.mcicon.com/wp-content/uploads/2021/01/Music_Movie_1-copy-2.jpg",
+  },
+  {
+    id: 6,
+    name: "Fast and Furious",
+    status: "watchlist",
+    image:
+      "https://www.mcicon.com/wp-content/uploads/2021/01/Music_Movie_1-copy-2.jpg",
+  },
+  {
+    id: 7,
+    name: "Awake",
+    status: "watchlist",
+    image:
+      "https://www.mcicon.com/wp-content/uploads/2021/01/Music_Movie_1-copy-2.jpg",
+  },
+  {
+    id: 8,
+    name: "Fatherhood",
+    status: "watchlist",
+    image:
+      "https://www.mcicon.com/wp-content/uploads/2021/01/Music_Movie_1-copy-2.jpg",
+  },
 ];
 export default movies;
